2670478e2f87ee819ef86931e7607bd7b1bdd39a
Lucas Martins <lucas.s.martins17@gmail.com>
2026-08-13 15:51:55 -0300

    feat: integrate data dirtying toggle into main pipeline

--- src/main.py
+++ src/main.py
@@ -11,11 +11,16 @@ from data_dirtying.vendas import sujar_vendas
 QTD_CLIENTES = 50
 QTD_PROFISSIONAIS = 5
 QTD_VENDAS = 200
-
+SUJAR_DADOS = False
 clientes = gerar_clientes(QTD_CLIENTES)
 profissionais = gerar_profissionais(QTD_PROFISSIONAIS)
 servicos = gerar_servico()
 vendas = gerar_vendas(QTD_VENDAS, clientes, profissionais, servicos)
+
+
+if SUJAR_DADOS:
+    clientes = sujar_clientes(clientes)
+    vendas = sujar_vendas(vendas)
 
 salvar_como_excel(clientes, '../data/raw/clientes.xlsx')
 salvar_como_excel(profissionais, '../data/raw/profissionais.xlsx')
